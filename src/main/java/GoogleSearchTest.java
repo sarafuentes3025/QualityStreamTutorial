8693b8802c23cc4d421f0f9e8de55c6996491623
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-08-18 12:04:11 -0500

    Inplement Fluent waits

--- src/main/java/GoogleSearchTest.java
+++ src/main/java/GoogleSearchTest.java
@@ -5,14 +5,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GoogleSearchTest {
 
-  public static WebDriver driver;
+  private static WebDriver driver;
+  By videoLinkLocator = By.cssSelector("a[href='https://www.youtube.com/watch?v=R_hh3jAqn8M']");
 
   @BeforeAll
   static void setUp() {
@@ -36,10 +39,18 @@ public class GoogleSearchTest {
 //    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 //    Explicit Wait
-    WebDriverWait eWait = new WebDriverWait(driver, 10);
-    eWait.until(ExpectedConditions.titleContains("quality-stream"));
+//    WebDriverWait eWait = new WebDriverWait(driver, 10);
+//    eWait.until(ExpectedConditions.titleContains("quality-stream"));
+//    assertThat(driver.getTitle()).contains("quality-stream Introducción a la Automatización de Pruebas de Software - Buscar con Google");
 
-    assertThat(driver.getTitle()).contains("quality-stream Introducción a la Automatización de Pruebas de Software - Buscar con Google");
+//    FluentWait
+    Wait<WebDriver> fWait = new FluentWait<>(driver)
+        .withTimeout(Duration.ofSeconds(10))
+        .pollingEvery(Duration.ofSeconds(2))
+        .ignoring(NoSuchFieldException.class);
+
+    WebElement video = fWait.until(webDriver -> driver.findElement(videoLinkLocator));
+    assertTrue(video.isDisplayed());
   }
 
   @AfterAll
