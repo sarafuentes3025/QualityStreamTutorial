ad5a3b83c627fa6832e5fe661e8679af017dc681
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-08-27 10:26:25 -0500

    Fix selectDropDownList_Passengers method

--- src/main/java/com/webElements/packg/DropDownList_Page.java
+++ src/main/java/com/webElements/packg/DropDownList_Page.java
@@ -3,7 +3,10 @@ package com.webElements.packg;
 import com.project.pom.Base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class DropDownList_Page extends Base {
 
@@ -12,7 +15,7 @@ public class DropDownList_Page extends Base {
 
   By flightsLinkLocator = By.linkText("Flights");
   By dropDownList_Passengers = By.name("passCount");
-  By dropDownList_DepartingFrom =  By.name("fromPort");
+  By dropDownList_DepartingFrom = By.name("fromPort");
 
   By userLocator = By.name("userName");
   By passLocator = By.name("password");
@@ -28,15 +31,32 @@ public class DropDownList_Page extends Base {
       type("qualityAdmin", userLocator);
       type("pass1", passLocator);
       click(signInBtnLocator);
+      click(flightsLinkLocator);
     } else {
       System.out.println("username textBox was not present");
     }
   }
 
-  public String selectDropDownList_Passengers(){
+  public String selectDropDownList_Passengers() {
+    WebElement dropDownList = findElement(dropDownList_Passengers);
+    List<WebElement> options = dropDownList.findElements(By.tagName("option"));
+    for (WebElement option : options) {
+      if (getText(option).equals("4")) {
+        click(option);
+      }
+    }
+
+    String selectedOption = "";
+    for (WebElement option : options) {
+      if (option.isSelected())
+        selectedOption = getText(option);
+    }
+    return selectedOption;
+  }
+
+  public String selectDropDownList_DepartingFrom() {
     Select selectList = new Select(findElement(dropDownList_DepartingFrom));
     selectList.selectByVisibleText("Paris");
     return getText(selectList.getFirstSelectedOption());
   }
-
 }
